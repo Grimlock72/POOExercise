3dfcbc22b059b4d3efe8e6fa1c08c09cfb86c3b4
Juan Carlos Cuellar Ruz <juan-carlos.cuellar@atos.net>
2022-02-22 15:17:56 -0600

    First commit of the POO exercise.

---
+++ ProgramacionOrientadaAObjetos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProgramacionOrientadaAObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsClientesBase cliente = new clsClientesBase(0, "Juan Carlos Cuéllar Ruz", "001", "CURJ721119KY9", 1, "Juan Carlos Cuéllar Ruz");
+
+            Console.WriteLine(cliente.Clave + "-" + cliente.Nombre);
+            Console.WriteLine(cliente.TipoRegimen);
+            Console.WriteLine(cliente.RFC);
+            Console.ReadKey();
+        }
+    }
+}
